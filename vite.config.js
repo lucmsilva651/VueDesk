5a0daa50c3d58ef35cf09eb2b0f3949d70e3aeb7
Luquinhas <lucmsilva651@gmail.com>
2025-11-08 00:50:25 -0300

    Removed unused Unplugin Icons import

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import Icons from 'unplugin-icons/vite'
 
 export default defineConfig({
   plugins: [
-    Icons({compiler: 'vue3'}),
     vue()
   ],
 })
